6680e33c218eb183210db32cb6551241242a2c56
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-09-09 21:44:31 -0300

    feat: create button and text field classes

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by André Barbosa on 09/09/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backGroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backGroundColor
+        self.setTitle(title, for: .normal)
+        
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
