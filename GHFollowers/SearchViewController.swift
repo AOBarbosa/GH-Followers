45bde4971897a6b9803c53c401ea82c29dad6cee
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-09-11 19:29:47 -0300

    feat: create custom alert dialog

--- GHFollowers/SearchViewController.swift
+++ GHFollowers/SearchViewController.swift
@@ -42,7 +42,8 @@ class SearchViewController: UIViewController {
     @objc func pushFollowerListVC() {
         
         guard isUsernameEntered else {
-            print("No username!")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 🙃!", buttonTitle: "Ok")
+            
             return
         }
         
---
+++ GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by André Barbosa on 11/09/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
