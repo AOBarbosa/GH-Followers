59b0e909046f96cb334a5d66593a0fdc5eca7b19
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-09-10 21:28:22 -0300

    feat: config get followers button

--- GHFollowers/SearchViewController.swift
+++ GHFollowers/SearchViewController.swift
@@ -17,8 +17,10 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
         configureLogoImageView()
         configureTextField()
+        configureCallToActionButton()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,4 +51,16 @@ class SearchViewController: UIViewController {
             usernameTextField.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
 }
